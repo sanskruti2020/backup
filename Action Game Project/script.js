5c2d92299f45a67c430e4b716d1460b9dff88609
Sanskruti Anil Jaiswal <93272910+sanskruti2020@users.noreply.github.com>
2023-08-02 05:52:15 +0530

    Add files via upload

---
+++ Action Game Project/script.js
@@ -0,0 +1,22 @@
+console.log("Welcome to my game!!")
+// if user press any key while playing -- taking thier key code
+document.onkeydown = function (e) {
+    console.log("Key code is: ", e.keyCode)
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
